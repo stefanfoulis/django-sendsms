a08a1e1d7281cf38e4a1b5c536ba96bd6e89b579
psychok7 <nunok7@gmail.com>
2017-05-09 20:47:59 +0100

    fix twilio import error

--- sendsms/backends/twiliorest.py
+++ sendsms/backends/twiliorest.py
@@ -1,13 +1,19 @@
 #-*- coding: utf-8 -*-
+
 """
 this backend requires the twilio python library: http://pypi.python.org/pypi/twilio/
 """
-from twilio.rest import TwilioRestClient
+try:
+    from twilio.rest import TwilioRestClient
+except ImportError:
+    from twilio.rest import Client as TwilioRestClient
+
 from django.conf import settings
 from sendsms.backends.base import BaseSmsBackend
 
 TWILIO_ACCOUNT_SID = getattr(settings, 'SENDSMS_TWILIO_ACCOUNT_SID', '')
 TWILIO_AUTH_TOKEN = getattr(settings, 'SENDSMS_TWILIO_AUTH_TOKEN', '')
+
 
 class SmsBackend(BaseSmsBackend):
     def send_messages(self, messages):
@@ -15,11 +21,11 @@ class SmsBackend(BaseSmsBackend):
         for message in messages:
             for to in message.to:
                 try:
-                    msg = client.messages.create(
+                    client.messages.create(
                         to=to,
                         from_=message.from_phone,
                         body=message.body
                     )
-                except:
+                except Exception as e:
                     if not self.fail_silently:
-                        raise
+                        raise Exception("Error: " + str(e))
