a7139db4d860b4dbfbdca7cc24f48694a7d8362f
Alican Toprak <tordox@gmail.com>
2018-12-19 17:05:12 +0100

    better error handling with error code explanations

--- sendsms/backends/nexmo.py
+++ sendsms/backends/nexmo.py
@@ -33,8 +33,32 @@ class AuthenticationError(ClientError):
     pass
 
 NEXMO_API_URL = 'https://rest.nexmo.com/sms/json'
-NEXMO_API_KEY = getattr(settings, 'NEXMO_API_KEY', '')
-NEXMO_API_SECRET = getattr(settings, 'NEXMO_PASSWORD', '')
+NEXMO_API_KEY = getattr(settings, 'SENDSMS_ACCOUNT_SID', '')
+NEXMO_API_SECRET = getattr(settings, 'SENDSMS_AUTH_TOKEN', '')
+
+
+nexmo_error_codes = {
+    1:  ["Throttled", "You have exceeded the submission capacity allowed on this account. Please wait and retry."],
+    2:  ["Missing params", "Your request is incomplete and missing some mandatory parameters."],
+    3: 	["Invalid params", "The value of one or more parameters is invalid."],
+    4:	["Invalid credentials", "The api_key / api_secret you supplied is either invalid or disabled."],
+    5: 	["Internal error", "There was an error processing your request in the Platform."],
+    6: 	["Invalid message", "The Platform was unable to process your request. For example, due to an unrecognised prefix for the phone number."],
+    7: 	["Number barred", "The number you are trying to submit to is blacklisted and may not receive messages."],
+    8: 	["Partner account barred", "The api_key you supplied is for an account that has been barred from submitting messages."],
+    9: 	["Partner quota exceeded", "Your pre-paid account does not have sufficient credit to process this message."],
+    11: ["Account not enabled for REST", "This account is not provisioned for REST submission, you should use SMPP instead."],
+    12: ["Message too long", "The length of udh and body was greater than 140 octets for a binary type SMS request."],
+    13: ["Communication Failed", "Message was not submitted because there was a communication failure."],
+    14: ["Invalid Signature", "Message was not submitted due to a verification failure in the submitted signature."],
+    15: ["Illegal Sender Address - rejected", "Due to local regulations, the SenderID you set in from in the request was not accepted. Please check the Global messaging section."],
+    16: ["Invalid TTL", "The value of ttl in your request was invalid."],
+    19: ["Facility not allowed", "Your request makes use of a facility that is not enabled on your account."],
+    20: ["Invalid Message class", "The value of message-class in your request was out of range. See https://en.wikipedia.org/wiki/Data_Coding_Scheme."],
+    23: ["Bad callback :: Missing Protocol", "You did not include https in the URL you set in callback."],
+    29: ["Non White-listed Destination", "The phone number you set in to is not in your pre-approved destination list. To send messages to this phone number, add it using Dashboard."],
+    34: ["Invalid or Missing Msisdn Param", "The phone number you supplied in the to parameter of your request was either missing or invalid."],
+}
 
 class SmsBackend(BaseSmsBackend):
     
@@ -62,55 +86,34 @@ class SmsBackend(BaseSmsBackend):
 
 
     def parse(self, host, response):
+        if not response.status_code == 200:
+            if self.fail_silently:
+                logger.warning(
+                    "Error: %s %r", response.status_code, response.content
+                )
+                return False
+            raise Error("Error: %s %r", response.status_code, response.content)
 
-        if response.status_code == 204:
-            return None
-        elif 200 <= response.status_code < 300:
-            content_mime = response.headers.get("content-type").split(";", 1)[0]
-            if content_mime == "application/json":
-                return response.json()
-            else:
-                return response.content
+        
+        status_code = int(response.json().get("messages")[0].get("status"))
 
+        if status_code == 0:
+            return True, response
+
+        error_type = nexmo_error_codes.get(status_code)
 
         if self.fail_silently:
             logger.warning(
-                "Client error: %s %r", response.status_code, response.content
-            )
-            return False
+                    "Error: %s %r", response.status_code, response.content
+                )
+            return False, requests
 
-        if response.status_code == 401:
-            raise AuthenticationError
-        elif 400 <= response.status_code < 500:
-            logger.warning(
-                "Client error: %s %r", response.status_code, response.content
-            )
-            message = "{code} response from {host}".format(
-                code=response.status_code, host=host
-            )
-            try:
-                error_data = response.json()
-                if (
-                    "type" in error_data
-                    and "title" in error_data
-                    and "detail" in error_data
-                ):
-                    message = "{title}: {detail} ({type})".format(
-                        title=error_data["title"],
-                        detail=error_data["detail"],
-                        type=error_data["type"],
-                    )
-            except JSONDecodeError:
-                pass
-            raise ClientError(message)
-        elif 500 <= response.status_code < 600:
-            logger.warning(
-                "Server error: %s %r", response.status_code, response.content
-            )
-            message = "{code} response from {host}".format(
-                code=response.status_code, host=host
-            )
-            raise ServerError(message)
+        raise ClientError("Error Code {status_code}: {text}:  {meaning}".format(
+                status_code=status_code,
+                text=error_type[0],
+                meaning=error_type[1],
+            ))
+
 
     def _send(self, message):
         """
@@ -125,15 +128,16 @@ class SmsBackend(BaseSmsBackend):
             'from': message.from_phone, 
             'to': ",".join(message.to),
             'text': message.body,
-            'api_key': self.get_api_key,
-            'api_secret': self.get_api_secret,
+            'api_key': self.get_api_key(),
+            'api_secret': self.get_api_secret(),
         }
- 
-        uri = "https://{api_uri}".format(api_uri=NEXMO_API_URL)
 
-        logger.debug("POST to %r with body: %r", uri, params)
 
-        return self.parse(uri, requests.post(uri, data=params))
+        print(params)
+
+        logger.debug("POST to %r with body: %r", NEXMO_API_URL, params)
+
+        return self.parse(NEXMO_API_URL, requests.post(NEXMO_API_URL, data=params))
 
     def send_messages(self, messages):
         """
@@ -145,7 +149,7 @@ class SmsBackend(BaseSmsBackend):
         """
         counter = 0
         for message in messages:
-            res = self._send(message)
+            res, _ = self._send(message)
             if res:
                 counter += 1
 
